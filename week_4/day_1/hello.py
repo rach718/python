c52942996b7eaf0293ae351f59a2e24f27d474b9
Rachel Jakob <racheljakob@Rachels-MacBook-Air-2.local>
2020-02-02 16:46:20 +0200

    python

---
+++ week_4/day_1/hello.py
@@ -0,0 +1,54 @@
+# CLASS:
+# nums = [1,3,5,7,9]
+# for num in nums:
+#     print(num)
+# print(num1 + num2)
+# print(name + " " + surname)
+
+# letters = ["a","b","c","d"]
+# for id, letter in enumerate(letters):
+#     print(id, letter)
+
+# if name == "jon":
+#     pass
+# elif name == "dave":
+#     pass
+# else:
+#     pass
+
+# EXERCISE1
+# hi = "hello world \n"
+# print(hi*4)
+
+# num = 99
+# num2 = 3
+# num3 = 8
+# print(num/num2 * num3)
+
+# name = "Rachel"
+# age = 18
+# shoe_size = 8
+# info  = input("At 18, my shoe_size remained 8: ok")
+# print(info)
+
+
+
+
+# my_age=18
+# year=123879
+# x=my_age+year
+# print("I will be " + str(x) + " in year 123879")
+
+# first_name = "Rachel"
+# last_name = "Jakob"
+# print(first_name+" "+last_name)
+
+# computer_brand = "MAC"
+# print("I have a" + " " + computer_brand)
+
+# # EXERCISE XP GOLD:
+# print("hello world\n"*4 + "I love python\n"*4)
+
+# x = input()
+# print(int(x) + int(x+x) + int(x+x+x) + int(x+x+x+x))
+
